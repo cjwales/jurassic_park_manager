d242489d4c8c687c6980bbefa5c77d0b4d2ce61f
cjwales <cjwales@gmail.com>
2019-11-30 18:59:18 +0000

    added autowire to parkrepository

--- server/src/main/java/com/codeclan/server/controllers/ParkController.java
+++ server/src/main/java/com/codeclan/server/controllers/ParkController.java
@@ -1,4 +1,14 @@
 package com.codeclan.server.controllers;
 
+import com.codeclan.server.repositories.parks.ParkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/parks")
 public class ParkController {
+
+    @Autowired
+    ParkRepository parkRepository;
 }
